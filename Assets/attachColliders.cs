5d2f9e75fabd285b5cac5e2c298049fd45cec5ca
Queue2109 <hj2109@student.uni-lj.si>
2024-03-24 17:04:00 +0100

    Piano physics added

---
+++ Assets/attachColliders.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachColliders : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            Debug.Log("Processing: " + child.gameObject.name);
+            
+            if (child.gameObject.GetComponent<BoxCollider>() == null)
+            {
+                BoxCollider newCollider = child.gameObject.AddComponent<BoxCollider>();
+                newCollider.size = new Vector3(0.001f, 0.01f, 0.001f);
+                Debug.Log("Added collider to: " + child.gameObject.name + " with size " + newCollider.size);
+            }
+            else
+            {
+                Debug.Log("Collider already exists on: " + child.gameObject.name);
+            }
+
+            if (child.gameObject.GetComponent<Rigidbody>() == null)
+            {
+                Rigidbody rb = child.gameObject.AddComponent<Rigidbody>();
+                rb.useGravity = false;
+                // rb.isKinematic = true;
+                Debug.Log("Added rigid body " + rb);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            Debug.Log("Key pressed: " + other.gameObject.name);
+        }
+    }
+}
